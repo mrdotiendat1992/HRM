beeab53aa15f165c31b0cdd03eabbe3d09b46c99
dotiendat <mrdotiendat1992>
2024-06-21 08:38:24 +0700

    Thêm thread để xử lý nhiều truy cập 1 lúc 2.2.2

--- app.py
+++ app.py
@@ -2929,4 +2929,4 @@ def export_dsxnk():
     
     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], f"xinnghikhac_{thoigian}.xlsx"), as_attachment=True)
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=81)
+    serve(app, host='0.0.0.0', port=81, threads=8)
