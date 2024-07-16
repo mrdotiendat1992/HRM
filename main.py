caaba2a89a5f133c34b0856601eec0668240cecf
dotiendat <mrdotiendat1992>
2024-07-16 17:06:34 +0700

    3.3

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from routes import *
 
 if __name__ == "__main__":
     app.logger.info("Khoi dong phan mem ...")
-    serve(app, host='0.0.0.0', port=81, threads=16)
+    # serve(app, host='0.0.0.0', port=81, threads=16)
+    app.run(host='0.0.0.0', port=81, debug=False)
