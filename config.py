5356c35b2ef52d1b73f4b102709458917baa90d4
viettran27 <viettranduc8996@gmail.com>
2024-11-11 11:13:45 +0700

    fix

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 # Tham số kết nối đển database (trong config flask, dùng cho phần đăng nhập sử dụng ORM với flask_sqlalchemy) 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 odbc_driver = "{ODBC Driver 17 for SQL Server}"
-database_server = "172.16.60.100;"
-database_name = "HR;"
-database_user = "huynguyen"
-database_password = "Namthuan@123"
+database_server = os.environ.get("DATABASE_SERVER")
+database_name = os.environ.get("DATABASE_NAME")
+database_user = os.environ.get("DATABASE_USER")
+database_password = os.environ.get("DATABASE_PASSWORD")
 
 # url kết nối với database (sử dụng khi dùng0 pyodbc)
-url_database_pyodbc = "Driver={SQL Server};Server=172.16.60.100;Database=HR;UID=huynguyen;PWD=Namthuan@123;"
+url_database_pyodbc = f"Driver={{SQL Server}};Server={database_server};Database={database_name};UID={database_user};PWD={database_password};"
