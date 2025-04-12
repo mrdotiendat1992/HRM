7edbae9ae3b015d95eba695f3abda1636a81a391
mrdotiendat <mrdotiendat@gmail.com>
2025-04-12 15:18:40 +0700

    fix

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from other_routes import *
 if __name__ == "__main__":
     while True:
         try:
-            flash("PRODUCT")
+            print("PRODUCT")
             serve(app, host="0.0.0.0", port=81, _quiet=True, threads=100)
         except subprocess.CalledProcessError as e:
-            flash(f"Flask gặp lỗi: {e}")
-            flash("Đang khởi động flask...")
+            print(f"Flask gặp lỗi: {e}")
+            print("Đang khởi động flask...")
             time.sleep(1)  # Đợi một khoảng thời gian trước khi khởi động lại
         except Exception as e:
-            flash(f"Lỗi không xác định: {e}")
-            flash("Đang khởi động lại flask...")
+            print(f"Lỗi không xác định: {e}")
+            print("Đang khởi động lại flask...")
             time.sleep(1)  # Đợi một khoảng thời gian trước khi khởi động lại
 
 
