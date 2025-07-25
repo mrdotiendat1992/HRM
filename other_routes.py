756032493e939b47976d352958b8af36dd705e14
mrdotiendat <mrdotiendat@gmail.com>
2025-07-25 09:20:10 +0700

    fix - 6.1

--- other_routes.py
+++ other_routes.py
@@ -1883,7 +1883,7 @@ def capnhatdieuchuyentheofile():
                     hople = kiemtra_thongtin_dieuchuyen(x,masothe,chucdanhmoi,chuyenmoi,loaidieuchuyen)
                     if not hople["ketqua"]:
                         flash(f"Dòng {x} sai thông tin: {hople['lydo']}")
-                        return redirect("/muc6_2") 
+                        return redirect("/muc6_1") 
                     else:
                         x += 1
                         
@@ -1985,8 +1985,7 @@ def capnhatdieuchuyentheofile():
                                                 capbaccu,hccategorycu,ngay)
                     flash("Cập nhật điều chuyển bằng file thành công !!!")
             except Exception as e:
-                print(e)
-                print(f"Cập nhật điều chuyển bằng file thất bại {e} !!!")
+                flash(f"Cập nhật điều chuyển bằng file thất bại {e} !!!")
     return redirect("/muc6_1")
 
 @app.route("/bangcong_hanhchinh_web", methods=["GET","POST"])
