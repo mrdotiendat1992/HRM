7390276e9f37b1a7cb8d2658a61ef71f246922ef
mrdotiendat <nqhuy.biz@gmail.com>
2025-12-18 13:24:42 +0700

    fix bug

--- app.py
+++ app.py
@@ -5834,7 +5834,7 @@ def lay_dulieu_chotcong(mst, tungay, denngay):
 def chaylaicong_hientai(mst, thang, nam):
 
     start_day = datetime(int(nam), int(thang), 1).strftime("%Y-%m-%d")
-    if thang < 12:
+    if int(thang) < 12:
         end_day = (datetime(int(nam), int(thang) + 1, 1) - timedelta(days=1)).strftime("%Y-%m-%d")
     else:
         end_day = (datetime(int(nam) + 1, 1, 1) - timedelta(days=1)).strftime("%Y-%m-%d")
@@ -5867,7 +5867,7 @@ def chaylaicong_hientai(mst, thang, nam):
 def chaylaicong_quakhu(mst, thang, nam):
 
     start_day = datetime(int(nam), int(thang), 1).strftime("%Y-%m-%d")
-    if thang < 12:
+    if int(thang) < 12:
         end_day = (datetime(int(nam), int(thang) + 1, 1) - timedelta(days=1)).strftime("%Y-%m-%d")
     else:
         end_day = (datetime(int(nam),int(thang),31)).strftime("%Y-%m-%d")
